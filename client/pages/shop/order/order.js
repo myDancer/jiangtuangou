f0a9f82b2534b32640d0a5eafe4da91e54053943
myDancer <326509877@qq.com>
2018-09-01 00:32:44 +0800

    增加详情页

---
+++ client/pages/shop/order/order.js
@@ -0,0 +1,23 @@
+// pages/shop/order/order.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    foodCard: [1,2,3,4,5]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
